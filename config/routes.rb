0a7d96a5b54432ed0143aa052766ad7c93f46863
eunbiggabi <eunbiggabi84@gmail.com>
2021-06-30 16:35:58 +1000

    sample products html and css

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   get "/home/about", to: "home#about"
-  # get "/products/index", to: "products#index"
-  # resources :products, only: [:show, :index]
-  # get "/products/:id", to: "products#show", as: "character"
-  get "/characters", to: "characters#index"
-  get "/characters/:id", to: "characters#show"
+  resources :products, only: [:show, :index]
   get "users/login", to: "users#index"
   get "users/create", to: "users#create"
   resource :users, only: [:index, :show]

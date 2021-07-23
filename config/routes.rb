63cce60853f1b8e2f06bce4e75c2d6b217420fc9
eunbiggabi <eunbiggabi84@gmail.com>
2021-07-23 22:36:09 +1000

    before make branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   get "/home/about", to: "home#about"
-  resources :products, only: [:show, :index]
   get "users/login", to: "users#index"
   get "users/create", to: "users#create"
+  get "users/profile", to: "users#profile"
   resource :users, only: [:index, :show]
+  resources :products, only: [:show, :index]
 end

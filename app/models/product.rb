cc59ab5217042b617ab26b9d9e95b26db47f4227
eunbiggabi <eunbiggabi84@gmail.com>
2021-07-25 16:01:55 +1000

    product html and css

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+
+    belongs_to :user
+
+    validates :name, presence: true, 
+                        uniqueness: { case_sensitive: false }, 
+                        length: { minimum: 6, maximum: 200 }
+
+    validates :description, presence: true,  
+                        length: { minimum: 6, maximum: 5000 }
 end
